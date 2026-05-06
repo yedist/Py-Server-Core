c0afb4c173fc372ef58e36ac9c9d93f1c0f71c0b
yedidya steinmetz <yedist10@gmail.com>
2026-05-06 21:27:37 +0300

    added server logs

--- src/py_server_core/server/server.py
+++ src/py_server_core/server/server.py
@@ -1,9 +1,16 @@
 import asyncio
 
+from ..logger import Logger
+
+
+# אין אימותים במתודות לפרמטרים, לאובייקטים שבאמת קיימים
 
 class Server:
-    def __init__(self, host: str, port: int):
+    def __init__(self, host: str, port: int, logger=None):
+        self._logger = logger or Logger()
         self._listen_address = (host, port)
+
+        self._server_id = self._logger.get_object_id()
         self._asyncio_server: asyncio.Server | None = None
 
     @property
@@ -21,8 +28,12 @@ class Server:
         # אין עמידות לשגיאות \ timeout
         self._asyncio_server = await asyncio.start_server(self._connection_reception, *self._listen_address)
 
+        address = [sock.getsockname() for sock in self._asyncio_server.sockets]
+        await self._logger.info("Server up", self._server_id, address=address)
+
     async def close(self):
         # אין עמידות לשגיאות \ timeout
         self._asyncio_server.close()
         await self._asyncio_server.wait_closed()
         self._asyncio_server = None
+        await self._logger.info("Server closed", self._server_id)  # exception?
