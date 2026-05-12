256dede8cee1297d0d0e2aab85951b60c5faff7f
yedidya steinmetz <yedist10@gmail.com>
2026-05-12 17:17:58 +0300

    Logging library integration, test changes

--- tests/server_object_test.py
+++ tests/server_object_test.py
@@ -1,37 +1,39 @@
 import asyncio
+import logging
 import socket
 
 from src.py_server_core import Server
+from test_tools import test_log_queue
 
 
 def initial():
-    logs = asyncio.Queue()
+    logs_queue, logs_handler = test_log_queue()
     server = Server(
         host="127.0.0.1",
         port=0,  # == the system will select a free port
-        logs_stream=logs
+        logs_handles=(logs_handler,)
     )
-    return server, logs
+    return server, logs_queue
 
 
 async def main():
-    server, logs = initial()
+    server, logs_queue = initial()
 
     await server.up()
     await server.close()
 
-    up_log, close_log = await logs.get(), await logs.get()
+    up_log, close_log = logs_queue.get(), logs_queue.get()
 
     # ['level'] test:
-    assert up_log["level"] == close_log["level"] == 'INFO'
+    assert up_log.levelno == close_log.levelno == logging.INFO
 
     # ['event'] test:
-    assert up_log["event"] == "Server up"
-    assert close_log["event"] == "Server closed"
+    assert up_log.message == "Server up"
+    assert close_log.message == "Server closed"
 
     # content test (up_log):
-    assert len(up_log["addresses"]) == 1
-    family, address = up_log["addresses"][0]
+    assert len(up_log.addresses) == 1
+    family, address = up_log.addresses[0]
 
     assert family == socket.AF_INET  # IPv4 ('127.0.0.1' is IPv4)
     host, port = address  # this is an IPv4 address structure.
