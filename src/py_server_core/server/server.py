4ce8e5a7fc5cdf031734ced2b63f6a72890cbd2b
yedidya steinmetz <yedist10@gmail.com>
2026-05-12 21:02:09 +0300

    changed name, types, and minor repairs

--- src/py_server_core/server/server.py
+++ src/py_server_core/server/server.py
@@ -1,26 +1,27 @@
 import asyncio
 import logging
+from collections.abc import Iterable
 
 from ..logs import LoggingCoordinator
 
 
 class Server:
-    def __init__(self, host: str, port: int, logs_handles: tuple[logging.Handler] = logging.NullHandler()):
+    def __init__(self, host: str, port: int, logs_handlers: Iterable[logging.Handler] = logging.NullHandler()):
         self._listen_address = (host, port)
         self._asyncio_server: asyncio.Server | None = None
 
-        log_service = LoggingCoordinator(*logs_handles)
+        log_service = LoggingCoordinator(*logs_handlers)
         self._logger = log_service.get_logger(__name__)
 
     @property
-    def in_work(self) -> bool:
-        return bool(self._asyncio_server)
+    def is_running(self) -> bool:
+        return self._asyncio_server is not None
 
     async def _connection_reception(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         pass
 
     async def up(self):
-        if self.in_work:
+        if self.is_running:
             return
 
         try:
@@ -38,13 +39,13 @@ class Server:
                 "Server up",
                 extra={
                     "addresses": [
-                        (sock.family, sock.getsockname()) for sock in self._asyncio_server.sockets
+                        (sock.family, sock.getsockname()) for sock in (self._asyncio_server.sockets or [])
                     ]
                 }
             )
 
     async def close(self):
-        if not self.in_work:
+        if not self.is_running:
             return
 
         try:
@@ -53,7 +54,7 @@ class Server:
             self._asyncio_server = None
         except Exception as exception:
             self._logger.error(
-                "Server up error",
+                "Server close error",
                 extra={
                     "exception_message": str(exception),
                     "exception_type": type(exception).__name__
