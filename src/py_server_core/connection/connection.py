261c475c9c1cd1accee49ea5f3a1faac03fa1aef
yedidya steinmetz <yedist10@gmail.com>
2026-05-14 14:19:47 +0300

    added setup of methods in connection.py

--- src/py_server_core/connection/connection.py
+++ src/py_server_core/connection/connection.py
@@ -1,10 +1,17 @@
 import asyncio
+from typing import Any
 
 
 class Connection:
     def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         self._reader = reader
         self._writer = writer
+
+    async def get(self) -> Any:
+        ...
+
+    async def send(self, data: bytes):
+        ...
 
     async def close(self):
         self._writer.close()
