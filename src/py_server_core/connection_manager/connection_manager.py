a8026eae8bbf95d44c44bfa5136974f49c9e59e8
yedidya steinmetz <yedist10@gmail.com>
2026-05-15 11:21:14 +0300

    added events model

--- src/py_server_core/connection_manager/connection_manager.py
+++ src/py_server_core/connection_manager/connection_manager.py
@@ -1,17 +1,25 @@
+from .default_events import connection_handler, on_resume_connection, on_close_connection
 from .counter import Counter
 
-
 class ConnectionManager:
-    def __init__(self, handler, max_connections: int):
+    def __init__(self, max_connections: int, events):
         self.max_connections = max_connections
         self._connections_counter = Counter(max_connections)
 
+        self._connection_handler = events.get("connection_handler", connection_handler)
+        self._on_close_connection = events.get("on_close_connection", on_close_connection)
+        self._on_resume_connection = events.get("on_resume_connection", on_resume_connection)
+
     async def new_connection(self, connection):
         if self._connections_counter.at_limit:
-            ...
+            await self._on_resume_connection(connection)
         else:
             await self._connections_counter.up()
             try:
-                await handler(connection)
+                await self._connection_handler(connection)
             finally:
                 await self._connections_counter.down()
+
+    async def close_connection(self, connection):
+        await self._on_close_connection(connection)
+        ...  # closing
---
+++ src/py_server_core/connection_manager/default_events.py
@@ -0,0 +1,3 @@
+async def connection_handler(connection): pass
+async def on_close_connection(connection): pass
+async def on_resume_connection(connection): pass
