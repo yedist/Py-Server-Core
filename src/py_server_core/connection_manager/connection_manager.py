6b6f0e0c9d509c57552f0cc77f00c4049356c15b
yedidya steinmetz <yedist10@gmail.com>
2026-05-15 11:29:56 +0300

    clean code

--- src/py_server_core/connection_manager/connection_manager.py
+++ src/py_server_core/connection_manager/connection_manager.py
@@ -1,26 +1,43 @@
-from .default_events import connection_handler, on_resume_connection, on_close_connection
+from .default_events import (
+    connection_handler,
+    on_resume_connection,
+    on_close_connection,
+)
 from .counter import Counter
+
 
 class ConnectionManager:
     def __init__(self, max_connections: int, events):
         self.max_connections = max_connections
-        self._connections_counter = Counter(max_connections)
+        self._counter = Counter(max_connections)
 
-        self._connection_handler = events.get("connection_handler", connection_handler)
-        self._on_close_connection = events.get("on_close_connection", on_close_connection)
-        self._on_resume_connection = events.get("on_resume_connection", on_resume_connection)
+        self._connection_handler = events.get(
+            "connection_handler",
+            connection_handler
+        )
+        self._on_close_connection = events.get(
+            "on_close_connection",
+            on_close_connection
+        )
+        self._on_resume_connection = events.get(
+            "on_resume_connection",
+            on_resume_connection
+        )
 
     async def new_connection(self, connection):
-        if self._connections_counter.at_limit:
+        if self._counter.at_limit:
             await self._on_resume_connection(connection)
-        else:
-            await self._connections_counter.up()
-            try:
-                await self._connection_handler(connection)
-            finally:
-                await self.close_connection(connection)
-                await self._connections_counter.down()
+            await self.close_connection(connection)
+            return
+
+        await self._counter.increment()
+
+        try:
+            await self._connection_handler(connection)
+        finally:
+            await self._on_close_connection(connection)
+            await self.close_connection(connection)
+            await self._counter.decrement()
 
     async def close_connection(self, connection):
-        await self._on_close_connection(connection)
         ...  # closing
--- src/py_server_core/connection_manager/counter.py
+++ src/py_server_core/connection_manager/counter.py
@@ -4,19 +4,23 @@ import asyncio
 class Counter:
     def __init__(self, limit):
         self.limit = limit
-
-        self.value = 0
+        self._value = 0
         self._lock = asyncio.Lock()
 
     @property
     async def at_limit(self):
         async with self._lock:
-            return self.limit <= self.value
+            return self._value >= self._limit
 
-    async def add(self):
+    @property
+    async def value(self):
         async with self._lock:
-            self.value = min(self.limit, self.value + 1)
+            return self._value
 
-    async def reduce(self):
+    async def increment(self):
         async with self._lock:
-            self.value = max(0, self.value - 1)
+            self._value = min(self.limit, self._value + 1)
+
+    async def decrement(self):
+        async with self._lock:
+            self._value = max(0, self._value - 1)
