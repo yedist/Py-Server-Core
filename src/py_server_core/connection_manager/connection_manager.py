fdcfb73cf9d1ea9d10d15c7cab47f7db7e6c8e43
yedidya steinmetz <yedist10@gmail.com>
2026-05-15 10:50:43 +0300

    started building connections limit

--- src/py_server_core/connection_manager/connection_manager.py
+++ src/py_server_core/connection_manager/connection_manager.py
@@ -1,2 +1,9 @@
 class ConnectionManager:
-    pass
+    def __init__(self, handler, max_connections: int):
+        self.max_connections = max_connections
+        self._connections_counter = 0
+
+    async def new_connection(self, connection):
+        self._connections_counter += 1
+        await handler(connection)
+        self._connections_counter -= 1
