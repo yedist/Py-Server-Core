55058458d192c6e0881cb0f24fc2f94dcb6bef81
yedidya steinmetz <yedist10@gmail.com>
2026-06-05 12:14:03 +0300

    added connections_counter in ConnectionManager

--- src/py_server_core/connection_manager/connection_manager.py
+++ src/py_server_core/connection_manager/connection_manager.py
@@ -3,10 +3,13 @@ from ..connection import Connection
 
 class ConnectionManager:
     def __init__(self):
-        ...
+        self.connections_counter = 0
 
     def new_connection(self, connection: Connection):
-        ...
+        self.connections_counter += 1
+
+    def close_connection(self, connection: Connection):
+        self.connections_counter -= 1
 
     def close_all_connections(self):
         ...
