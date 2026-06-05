fba95b6293262b780000ba9657b5fe61b0e4bf5c
yedidya steinmetz <yedist10@gmail.com>
2026-06-05 14:12:57 +0300

    added max_connections mechanism

--- src/py_server_core/connection_manager/connection_manager.py
+++ src/py_server_core/connection_manager/connection_manager.py
@@ -1,18 +1,23 @@
+from typing import Final
+
 from ..connection import Connection
 
 
 class ConnectionManager:
-    def __init__(self):
+    def __init__(self, max_connections: int | None):
+        self.max_connections: Final[int | float] = max_connections or float('inf')
         self._all_connections = set()
 
+    @property
     async def num_connections(self):
         return len(self._all_connections)
 
     async def registration(self, connection: Connection):
-        self._all_connections.add(connection)
+        if self.max_connections < self.num_connections:
+            self._all_connections.add(connection)
 
     async def unregistration(self, connection: Connection):
-        self._all_connections.remove(connection)
+        self._all_connections.discard(connection)
 
     def close_all_connections(self):
         ...
